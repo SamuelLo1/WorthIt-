bfe84dbe6ae74f46f11657046db750c6dee6561e
SamuelLo <samuello300@gmail.com>
2024-10-13 23:44:20 -0700

    added FastAPI endpoint for priceAnalysis

--- Backend/main.py
+++ Backend/main.py
@@ -12,6 +12,9 @@ from pydantic import BaseModel
 import base64
 from io import BytesIO
 
+import google.generativeai as genai
+from google.ai.generativelanguage_v1beta.types import content
+
 # Load the .env file
 load_dotenv()
 
@@ -78,6 +81,7 @@ def retrieve_item(item: str):
 class ImageData(BaseModel):
     base64Image: str
 
+# OCR endpoint to extract text from image and use that for prompting
 @app.post("/upload-base64")
 async def process_image(image_data: ImageData):
     print("trigged image endpoint")
@@ -102,7 +106,43 @@ async def process_image(image_data: ImageData):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
+    
 
+genai.configure(api_key=os.getenv("GEMINI_KEY"))
+
+generation_config = {
+  "temperature": 1.25,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_schema": content.Schema(
+    type = content.Type.OBJECT,
+    properties = {
+      "similarProds": content.Schema(
+        type = content.Type.ARRAY,
+        items = content.Schema(
+          type = content.Type.STRING,
+        ),
+      ),
+      "worthOrNot": content.Schema(
+        type = content.Type.BOOLEAN,
+      ),
+      "details": content.Schema(
+        type = content.Type.STRING,
+      ),
+    },
+  ),
+  "response_mime_type": "application/json",
+}
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+)
+@app.get("/productAnalysis/")
+async def gen_analysis(itemName:str, currentType: str, translateType: str, price: str):
+    prompt = (f"I am buying {itemName} and I am in a region where {currentType} currency is used, the cost of {itemName} is {price} {currentType}, if I am originally from the region where {translateType} is used. Generate details of if the product is worth it considering prices of {itemName} in the region where I am from and region of {currentType} also consider comparing the price with online pricing, but take a firm bias in a string and provide an array of similar products. Finally provide a boolean of worthIt or not based on the bias")
+    response = model.generate_content(prompt)
+    return response
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
